854aba6ead578772c234081e61a9fa5465bd9524
Yash Tyagi <140545580+yashgithub1111@users.noreply.github.com>
2025-02-19 23:34:41 +0530

    Add files via upload

---
+++ backend/models/Task.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const TaskSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  title: { type: String, required: true },
+  description: { type: String },
+  completed: { type: Boolean, default: false },
+});
+
+const Task = mongoose.model('Task', TaskSchema);
+module.exports = Task;
